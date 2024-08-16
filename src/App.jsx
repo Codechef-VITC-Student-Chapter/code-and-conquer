2b5748cfe75162e525ae5d5db72b9274ff046c0c
poseidon0z <adityaprabhu1290@gmail.com>
2024-08-16 22:12:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HomePage from './screens/HomePage';
+
+function App() {
+  return (
+    <>
+      <HomePage></HomePage>
+    </>
+  );
+}
+
+export default App;
---
+++ src/screens/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="flex justify-center items-center h-screen text-6xl font-bold text-center">
+      Code and Conquer
+    </div>
+  );
+}
+
+export default HomePage;
