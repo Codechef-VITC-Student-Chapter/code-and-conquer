cd7e2136eff601ffb3a5e05fc8eefa71111a8ed5
Aditya <aditya.2023@vitstudent.com>
2024-08-19 20:30:43 +0530

    Updated the sponsors page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        monoton: ['Monoton', 'cursive'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
+
+
