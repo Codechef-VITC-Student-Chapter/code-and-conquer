5df5c4f757f4f7072202478c346dc0c99a8976d2
Azaken1248 <suvkrm@gmail.com>
2024-08-21 01:10:23 +0530

    Added FAQBox Component

---
+++ src/components/FAQBox.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import '../styles/faq.css';
+import arrow from '../assets/faq/arrow.png'; // Import the arrow image
+
+const FAQBox = ({ question, answer }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="relative w-[80vw] mb-5">
+            <div
+                className="faq-box-bg w-full h-[9vh] py-4 px-6 text-left pl-[3rem] text-white rounded-t-md focus:outline-none font-montserrat font-semibold text-2xl flex justify-between items-center">
+                {question}
+                <button onClick={toggleDropdown} className="ml-4 text-xl focus:outline-none">
+                    <img
+                        src={arrow}
+                        alt="Arrow"
+                        className={`w-[40px] h-[24px] transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+                    />
+                </button>
+            </div>
+            {isOpen && (
+                <div className="faq-box-bg top-full left-0 w-full bg-opacity-50 rounded-b-md pl-[1.5rem] mb-5">
+                    <div className="w-[80%] mx-auto border-t-2 border-black"></div>
+                    <p className="text-white font-montserrat text-xl px-6 py-2">{answer}</p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default FAQBox;
