dde3721c88768ccf16fce0d62d1cbb5afa7bbc7c
Azaken1248 <suvkrm@gmail.com>
2024-08-21 01:10:51 +0530

    Added the FAQ Screen

--- src/screens/FAQs.jsx
+++ src/screens/FAQs.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
+import FAQBox from '../components/FAQBox';
+
+import '../styles/faq.css';
+
 const FAQs = () => {
   return (
-    <div className="flex flex-col items-center justify-center faq-bg">
-      <div id="faqs" className="text-white text-7xl h-[100vh]">
-        <span className="font-monoton text-[40px] md:text-[75px] font-normal  md:leading-[140px] leading-[100px] text-center uppercase text-white">
+    <div
+      className="faq-bg-img flex flex-col items-center">
+      <div id="faqs" className="text-white text-7xl h-full flex flex-col items-center justify-center">
+        <span className="font-monoton text-[40px] md:text-[75px] font-normal md:leading-[140px] leading-[100px] text-center">
           FAQs
         </span>
+        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
+        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
+        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
+        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
+        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
+        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
+        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
+        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
+        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
       </div>
     </div>
   );
