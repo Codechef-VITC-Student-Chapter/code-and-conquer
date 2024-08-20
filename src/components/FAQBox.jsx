03730ae6ce76316cdc817831951d1483e20e5fca
Azaken1248 <suvkrm@gmail.com>
2024-08-21 01:46:18 +0530

    Fixed Fonts

--- src/components/FAQBox.jsx
+++ src/components/FAQBox.jsx
@@ -12,7 +12,7 @@ const FAQBox = ({ question, answer }) => {
     return (
         <div className="relative w-[80vw] mb-5">
             <div
-                className="faq-box-bg w-full h-[9vh] py-4 px-6 text-left pl-[3rem] text-white rounded-t-md focus:outline-none font-montserrat font-semibold text-2xl flex justify-between items-center">
+                className="faq-box-bg w-full h-[9vh] py-4 px-6 text-left pl-[3rem] text-white rounded-t-md focus:outline-none font-montserrat font-semibold text-2xl md:text-xl sm:text-lg flex justify-between items-center">
                 {question}
                 <button onClick={toggleDropdown} className="ml-4 text-xl focus:outline-none">
                     <img
@@ -25,7 +25,7 @@ const FAQBox = ({ question, answer }) => {
             {isOpen && (
                 <div className="faq-box-bg top-full left-0 w-full bg-opacity-50 rounded-b-md pl-[1.5rem] mb-5">
                     <div className="w-[80%] mx-auto border-t-2 border-black"></div>
-                    <p className="text-white font-montserrat text-xl px-6 py-2">{answer}</p>
+                    <p className="text-white font-montserrat text-xl md:text-lg sm:text-base px-6 py-2">{answer}</p>
                 </div>
             )}
         </div>
