5702933100ea1b1d5ce232ad23d1a361f4558c94
poseidon0z <adityaprabhu1290@gmail.com>
2024-08-21 19:46:32 +0530

    display improvements

--- src/components/DiamondLine.jsx
+++ src/components/DiamondLine.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
 const DiamondLine = () => {
-  const positions = [160, 460, 760, 1060, 1360];
+  const positions = [136, 460, 760, 1060, 1360];
 
   return (
     <>
       {/* Line */}
-      <div className="absolute left-40 top-1/2 -translate-y-1/2 h-1 bg-white w-[1200px]"></div>
-
+      <div className="absolute left-36 top-1/2 -translate-y-1/2 h-1 bg-white w-[1200px]"></div>
       {/* Diamonds */}
-      {positions.map((left, index) => (
+      {[...Array(5)].map((_, index) => (
         <div
           key={index}
-          style={{ left: `${left}px` }}
+          style={{ left: `${index * 300 + 136}px` }}
           className="absolute h-3 w-3 top-1/2 -translate-y-1/2 rotate-45 bg-cyan-300"
         ></div>
       ))}
--- src/components/TimelineCard.jsx
+++ src/components/TimelineCard.jsx
@@ -7,7 +7,7 @@ function TimelineCard({ title, description, date, orange }) {
         orange
           ? `from-[#240422] from-65% to-orange-400`
           : `from-[#240422] from-65% to-blue-900`
-      } text-white p-4 rounded-lg border-2 border-dashed border-white shadow-lg w-56 my-4`}
+      } text-white p-4 rounded-lg border-2 border-dashed border-white shadow-lg w-60 my-4 h-48`}
     >
       <h3 className="text-lg font-bold m-2 text-center">{title}</h3>
       <p className="text-sm text-center mb-3">{description}</p>
