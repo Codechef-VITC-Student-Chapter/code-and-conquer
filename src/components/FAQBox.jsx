b04b42b24df012a5691b0f432e9fe984ee143d83
poseidon0z <adityaprabhu1290@gmail.com>
2024-08-24 22:59:02 +0530

    UI Improvements

--- src/components/FAQBox.jsx
+++ src/components/FAQBox.jsx
@@ -3,34 +3,42 @@ import '../styles/faq.css';
 import arrow from '../assets/faq/arrow.png'; // Import the arrow image
 
 const FAQBox = ({ question, answer }) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div className="relative w-[80vw] mb-5">
-            <div
-                onClick={toggleDropdown}
-                className="faq-box-bg cursor-pointer w-full h-[9vh] py-4 px-6 text-left pl-[3rem] text-white rounded-t-md focus:outline-none font-montserrat font-semibold text-xl md:text-xl sm:text-lg flex justify-between items-center">
-                {question}
-                <button onClick={toggleDropdown} className="ml-4 text-xl focus:outline-none">
-                    <img
-                        src={arrow}
-                        alt="Arrow"
-                        className={`w-[40px] h-[24px] transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
-                    />
-                </button>
-            </div>
-            {isOpen && (
-                <div className="faq-box-bg top-full left-0 w-full bg-opacity-50 rounded-b-md pl-[1.5rem] mb-5">
-                    <div className="w-[80%] mx-auto border-t-2 border-black"></div>
-                    <p className="text-white font-montserrat text-xl md:text-lg sm:text-base px-6 py-2">{answer}</p>
-                </div>
-            )}
+  return (
+    <div className="relative w-[80vw] mb-5">
+      <div
+        onClick={toggleDropdown}
+        className="faq-box-bg cursor-pointer w-full h-[9vh] py-4 px-6 text-left pl-[3rem] text-white rounded-t-md focus:outline-none font-montserrat font-semibold text-sm sm:text-l md:text-xl flex justify-between items-center"
+      >
+        {question}
+        <button
+          onClick={toggleDropdown}
+          className="ml-4 text-xl focus:outline-none"
+        >
+          <img
+            src={arrow}
+            alt="Arrow"
+            className={`w-[40px] h-[24px] transition-transform duration-300 ${
+              isOpen ? 'rotate-180' : 'rotate-0'
+            }`}
+          />
+        </button>
+      </div>
+      {isOpen && (
+        <div className="faq-box-bg top-full left-0 w-full bg-opacity-50 rounded-b-md pl-[1.5rem] mb-5">
+          <div className="w-[80%] mx-auto border-t-2 border-black"></div>
+          <p className="text-white font-montserrat text-sm sm:text-l md:text-xl px-6 py-2">
+            {answer}
+          </p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default FAQBox;
--- src/screens/AboutUs.jsx
+++ src/screens/AboutUs.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div className="flex flex-col items-center justify-center reverse-wrapper-bg min-h-[100vh] z-1" id="about-us">
-      <div className="w-full flex justify-center"> 
+    <div
+      className="flex flex-col items-center justify-center reverse-wrapper-bg min-h-[100vh] z-1"
+      id="about-us"
+    >
+      <div className="w-full flex justify-center">
         <span className="font-monoton text-[40px] md:text-[75px] font-normal md:leading-[140px] leading-[100px] text-white uppercase">
           About &nbsp;&nbsp;&nbsp;&nbsp; us
         </span>
       </div>
-      <div className="font-montserrat text-m w-[75vw] text-left text-white mt-14"> 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div className="font-montserrat text-m w-[75vw] text-left text-white mt-14">
+        Get ready to elevate your skills with "Code & Conquer"—a thrilling
+        two-day workshop where web development meets cybersecurity! On day one,
+        dive into essential skills like Git, backend, and frontend development.
+        Day two focuses on web security, teaching you to identify and prevent
+        common vulnerabilities, all leading to an exciting Capture the Flag
+        (CTF) challenge. With hands-on learning, expert guidance, networking
+        opportunities, and a certification to boost your credentials, this event
+        is your gateway to innovating and securing the digital landscape. Don’t
+        miss out on this action-packed experience!
       </div>
-      <div className="flex flex-col md:flex-row items-center w-[75vw] text-white mt-14"> {/* Flex container */}
+      <div className="flex flex-col md:flex-row items-center w-[75vw] text-white mt-14">
+        {' '}
+        {/* Flex container */}
         <img
           width={180}
           height={180}
@@ -20,18 +33,27 @@ const AboutUs = () => {
           className="mb-4 md:mb-0 md:mr-4"
         />
         <p className="font-montserrat text-m md:ml-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          The CodeChef VITC Student Chapter at VIT Chennai is a dynamic
+          community dedicated to enhancing coding and problem-solving skills. We
+          host coding contests, hackathons, and workshops on algorithms, data
+          structures, web development, AI, and ML. Our mission is to foster
+          continuous learning, collaboration, and excellence in programming.
         </p>
       </div>
-      <div className="flex flex-col-reverse md:flex-row items-center w-[75vw] text-white mt-14"> {/* Flex container */}
+      <div className="flex flex-col-reverse md:flex-row items-center w-[75vw] text-white mt-14">
+        {' '}
+        {/* Flex container */}
         <p className="font-montserrat text-m md:mr-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          CYSCOM, previously OWASP VIT, is AICTE-recognized under the SPICES
+          scheme, dedicated to promoting cybersecurity awareness. It aims to
+          create a safer digital world by educating the public on preventing
+          cyber threats through various events and initiatives.
         </p>
         <img
           width={180}
           height={180}
           src="https://res.cloudinary.com/dxu5hlgvd/image/upload/v1720609534/FINAL_LOGO-CMZ27HCT_rvrnax.png"
-          alt="codechefVITC_logo"
+          alt="cyscom_logo"
           className="mt-4 md:mt-0 md:ml-4"
         />
       </div>
