2e464c6d0d3af6207f0e3c9e16b680b147a40ff3
Akkilesh-A <akkilalagar05@gmail.com>
2024-09-04 06:12:59 +0530

    added faqs

--- src/screens/FAQs.jsx
+++ src/screens/FAQs.jsx
@@ -12,10 +12,10 @@ const FAQs = () => {
         <span className="font-monoton text-[40px] md:text-[75px] font-normal md:leading-[140px] leading-[100px] text-center">
           FAQs
         </span>
-        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
-        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
-        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
-        <FAQBox question={"In mi nulla, fringilla vestibulum?"} answer={"In mi nulla, fringilla vestibulum?"}></FAQBox>
+        <FAQBox question={"🚀 New to Web Development and Cybersecurity?  🚀"} answer={"Yes, you can absolutely take part in this event! 🌟 We’ve got a series of completely free online events preceding the main event to get you up to speed, check the time line above and got click on the big blue register button 😉"}></FAQBox>
+        <FAQBox question={"Worried about overlapping with SIH? 😓 "} answer={"No need to worry! This event won't conflict with SIH; we have confirmed with SWC!  🎉"}></FAQBox>
+        <FAQBox question={"Is this an overnight event? 🌙"} answer={"Yes, this event will be held overnight! 🕒"}></FAQBox>
+        <FAQBox question={" Individual or Group? 👥"} answer={"You can register individually, but you’ll have the chance to form groups on the spot to participate in the CTFs! "}></FAQBox>
       </div>
     </div>
   );
